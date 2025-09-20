d8bec1cc15e03b110d914ee1ebaa3e8cc6ab4f1b
Azu Chukwuemeka <azuchukwuemeka9@gmail.com>
2025-09-20 05:59:20 -0700

    Delete .next directory

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"PlDz77WTJFjIPwM9vWasB0rHaLoDhthXjpmgGpFy9BU=\"\n}"
--- .next/static/chunks/Documents_GitHub_Fluxrware-rebrand-website_src_app_layout_tsx_1cc4ebc1._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__e1b0c44a._.css",
-  "static/chunks/_02129c51._.js",
-  "static/chunks/Documents_GitHub_Fluxrware-rebrand-website_src_theme_ts_1a4801ed._.js"
-],
-    source: "dynamic"
-});
