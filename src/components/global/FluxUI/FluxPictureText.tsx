675918cde7ab786c806a5e85eb9fe22af65ef56f
Azu Chukwuemeka <azuchukwuemeka9@gmail.com>
2025-09-24 05:14:02 -0700

    Update FluxPictureText.tsx

--- src/components/global/FluxUI/FluxPictureText.tsx
+++ src/components/global/FluxUI/FluxPictureText.tsx
@@ -1,9 +1,6 @@
 import { Box } from "@mui/material";
 import React from "react";
-type picture = {
-    children: any
-}
-export function FluxPictureText(props: picture) {
+export function FluxPictureText(props: any) {
     return (
         <Box display={"flex"} paddingBlock={1} alignItems={"center"}>
             {props.children}
