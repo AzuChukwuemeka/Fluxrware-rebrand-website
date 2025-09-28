221ad8520e57e8f9cd49a528486ca146ae842611
Azu Chukwuemeka <azuchukwuemeka9@gmail.com>
2025-09-28 04:07:58 -0700

    Animation for whatweoffer

--- src/components/landingPage/WhatWeOffer.tsx
+++ src/components/landingPage/WhatWeOffer.tsx
@@ -7,38 +7,65 @@ import { useGSAP } from "@gsap/react";
 import { useRef } from "react";
 import { slideBottomDefaultState, slideBottomToProperties } from "@/animations/slideBottom";
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function WhatWeOffer() {
-  const titleRef = useRef<HTMLDivElement | null>(null);
+  const offerTitleRef = useRef<HTMLDivElement | null>(null);
+
   useGSAP(() => {
-    const timeline = gsap.timeline();
-    gsap.set(titleRef, slideBottomDefaultState);
-    timeline.to(titleRef.current, slideBottomToProperties)
+    const timeline = gsap.timeline({
+      defaults: {
+        duration: 0.4,
+        ease: "expo.inOut",
+      }
+    });
+
+    // Initial state
+    gsap.set(offerTitleRef.current, slideBottomDefaultState);
+
+    // Animate on scroll
+    timeline.to(offerTitleRef.current, {
+      ...slideBottomToProperties,
+      scrollTrigger: {
+        trigger: offerTitleRef.current,
+        start: "top bottom",
+        end: "bottom 90%",
+        scrub: true,
+      }
+    });
+
   }, []);
+
   const theme: Theme = useTheme();
+
   return (
-    <>
-      <Box style={{ background: "radial-gradient(circle at bottom left, #fefefe 94%, lightblue) 200% 200%" }}>
-        <Container 
-          maxWidth="lg" 
-          ref = {titleRef}
-          sx = {{visibility: "hidden"}}
+    <Box style={{ background: "radial-gradient(circle at bottom left, #fefefe 94%, lightblue) 200% 200%" }}>
+      <Container maxWidth="lg">
+        <Typography
+          variant="h3"
+          textAlign="center"
+          paddingTop={theme.spacing(10)}
+          fontWeight="bold"
+          ref={offerTitleRef}
+          sx={{ visibility: "hidden" }}
         >
-          <Typography variant="h3" textAlign={"center"} paddingTop={theme.spacing(10)} fontWeight="bold">
-            What We <span style={{ color: theme.palette.primary.main }}>Offer</span>
-          </Typography>
-          <Typography variant="body1" textAlign={"center"} paddingTop={theme.spacing(4)} paddingBottom={10}>
-            Solutions tailored to grow your business from the ground up.
-          </Typography>
-          <Grid container spacing={theme.spacing(10)} alignItems={"center"}>
-            {whatWeOffer.map((value, index) => {
-              return (
-                <GridAlternate key={value.title} item={value} flow={((index + 1) % 2 == 0) ? false : true} />
-              )
-            })}
-          </Grid>
-        </Container>
-      </Box>
-    </>
-  )
+          What We <span style={{ color: theme.palette.primary.main }}>Offer</span>
+        </Typography>
+        <Typography variant="body1" textAlign="center" paddingTop={theme.spacing(4)} paddingBottom={10}>
+          Solutions tailored to grow your business from the ground up.
+        </Typography>
+        <Grid container spacing={theme.spacing(10)} alignItems="center">
+          {whatWeOffer.map((value, index) => (
+            <GridAlternate
+              key={value.title}
+              item={value}
+              flow={(index + 1) % 2 !== 0}
+            />
+          ))}
+        </Grid>
+      </Container>
+    </Box>
+  );
 }
